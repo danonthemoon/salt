5ae47c288422d48c123d47c4d46339be445685d1
Danny Sallurday <dannysallurday@gmail.com>
2019-07-02 16:15:21 +0200

    switch plt backend

--- saltefficiency/daterangestats.py
+++ saltefficiency/daterangestats.py
@@ -14,6 +14,7 @@ import numpy as np
 import sdb_mysql as mysql
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 import run_blockvisitstats
 from nightstats import getnightstats
