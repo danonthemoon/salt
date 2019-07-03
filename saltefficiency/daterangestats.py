97ad97c18f90e3b90bb73b9b6565efe30de26d46
Danny Sallurday <dannysallurday@gmail.com>
2019-07-03 11:27:37 +0200

    bar label fix

--- saltefficiency/daterangestats.py
+++ saltefficiency/daterangestats.py
@@ -72,8 +72,8 @@ if __name__=='__main__':
         # set individual bar lables using above list
        for i in ax.patches:
            # get_x pulls left or right; get_height pushes up or down
-           ax.text(i.get_x(), i.get_height()-0.5, \
-                    str(round(i.get_height(), 2)), fontsize=18,
+           ax.text(i.get_x()-.2, i.get_height(), \
+                    str(round(i.get_height(), 0)), fontsize=18,
                         color='white')
        ax.set_ylabel("Time (s)")
        ax.set_xticklabels(['Average Overheads'], rotation='horizontal')
