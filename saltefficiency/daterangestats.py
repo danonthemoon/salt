e0bcab2ef171ad373b2058b0c32b3c0527d05a9c
Danny Sallurday <dannysallurday@gmail.com>
2019-07-12 00:21:39 +0200

    charged lines on plot

--- saltefficiency/daterangestats.py
+++ saltefficiency/daterangestats.py
@@ -132,6 +132,14 @@ if __name__=='__main__':
                    str(round(sum(hrs_heights),1))+' (total)', fontsize=14, horizontalalignment='center',
                         color='black', fontweight='bold')
 
+       #charged lines
+       ax.text(0, 600, \
+                   '600 --------------------', fontsize=14, horizontalalignment='center',
+                        color='black', fontweight='bold')
+       ax.text(1, 500, \
+                   '-------------------- 500', fontsize=14, horizontalalignment='center',
+                        color='black', fontweight='bold')
+
        #plot appearance
        ax.set_ylabel("Time (s)", fontweight='bold')
        ax.set_yticks(np.arange(0,1050,50))
