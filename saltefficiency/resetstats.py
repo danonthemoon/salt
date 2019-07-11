88abbb3b0033ba9e2d8ecb0f8797fbfa48e1774a
Danny Sallurday <dannysallurday@gmail.com>
2019-07-11 23:01:03 +0200

    moved reset

--- saltefficiency/resetstats.py
+++ saltefficiency/resetstats.py
@@ -6,6 +6,24 @@ import sdb_mysql as mysql
 
 from blockvisitstats import getnightinfo
 
+def resetstats(sdb, obsdate):
+   #for a given obsdate get the night info
+   nid=getnightinfo(sdb, obsdate)
+
+   selcmd='BlockVisit_Id, BlockVisitStatus_Id, Proposal_Code, Block_Id'
+   tabcmd='Block join BlockVisit using (Block_Id) join Proposal using (Proposal_Id) join ProposalCode on (Proposal.ProposalCode_Id = ProposalCode.ProposalCode_Id)'
+   blocks=sdb.select(selcmd, tabcmd, 'NightInfo_Id=%i' % nid)
+   bvid_list=[]
+   for b in blocks:
+       if b[1]==1:
+          bvid_list.append(b[0])
+   for bvid in bvid_list:
+      inscmd='SlewTime=0, TrackerSlewTime=0, TargetAcquisitionTime=0'
+      sdb.update(inscmd, 'BlockVisit', 'BlockVisit_Id=%i' % bvid)
+      inscmd='InstrumentAcquisitionTime=0, ScienceTrackTime=0'
+      sdb.update(inscmd, 'BlockVisit', 'BlockVisit_Id=%i' % bvid)
+   return bvid_list
+   
 if __name__=='__main__':
    """Determine the overhead statistics for a specific observation date.
 
@@ -27,23 +45,3 @@ if __name__=='__main__':
        print (obsdate)
        results=resetstats(sdb, obsdate)
        date += datetime.timedelta(days=1)
-
-
-
-def resetstats(sdb, obsdate):
-   #for a given obsdate get the night info
-   nid=getnightinfo(sdb, obsdate)
-
-   selcmd='BlockVisit_Id, BlockVisitStatus_Id, Proposal_Code, Block_Id'
-   tabcmd='Block join BlockVisit using (Block_Id) join Proposal using (Proposal_Id) join ProposalCode on (Proposal.ProposalCode_Id = ProposalCode.ProposalCode_Id)'
-   blocks=sdb.select(selcmd, tabcmd, 'NightInfo_Id=%i' % nid)
-   bvid_list=[]
-   for b in blocks:
-       if b[1]==1:
-          bvid_list.append(b[0])
-   for bvid in bvid_list:
-      inscmd='SlewTime=0, TrackerSlewTime=0, TargetAcquisitionTime=0'
-      sdb.update(inscmd, 'BlockVisit', 'BlockVisit_Id=%i' % bvid)
-      inscmd='InstrumentAcquisitionTime=0, ScienceTrackTime=0'
-      sdb.update(inscmd, 'BlockVisit', 'BlockVisit_Id=%i' % bvid)
-   return bvid_list
