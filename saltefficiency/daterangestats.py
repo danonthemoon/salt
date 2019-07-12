5fd2c787d8487d4a236a5bf5e7473b15df72e50b
Danny Sallurday <dannysallurday@gmail.com>
2019-07-12 18:35:04 +0200

    better looking plot

--- saltefficiency/daterangestats.py
+++ saltefficiency/daterangestats.py
@@ -146,7 +146,7 @@ if __name__=='__main__':
        #plot appearance
        ax.set_ylabel("Time (s)", fontweight='bold')
        ax.set_yticks(np.arange(0,1050,50))
-       ax.set_xticklabels(['RSS (%i blocks)' % rssblocks, 'HRS (%i blocks)' % hrsblocks], rotation='horizontal', fontweight='bold')
+       ax.set_xticklabels(['RSS\n (%i blocks)' % rssblocks, 'HRS\n (%i blocks)' % hrsblocks], rotation='horizontal', fontweight='bold')
        ax.set_title('Overhead Statistics for %s to %s' % (sdate,edate),fontweight='bold')
        ax.legend(loc=0, fontsize=12)
        pdf.savefig() # saves the current figure into a pdf page
