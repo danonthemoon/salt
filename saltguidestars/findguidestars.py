8722d45a781fb50458da0b4d4b9c9a8ec59aed09
Danny Sallurday <dannysallurday@gmail.com>
2019-07-29 16:07:32 +0200

    prep for guidestar

---
+++ saltguidestars/findguidestars.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+target_rah
+target_ram
+target_ras
+
+target_decsign
+target_decm
+target_decs
+
+#find rectangle for probe range and ring between RSS and SCAM. Stars must be within both, at a theta > 45 deg
+#between each other. Preferable to have them directly opposite. magnitude above 21 and below 9. no binaries.
